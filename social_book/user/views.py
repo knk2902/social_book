4ea8ea6a31a7b73dd9aec42bbd9ede4a72bf9ecf
knk2902 <khachanekn29@gmail.com>
2023-07-03 14:16:26 +0530

    version 1

---
+++ social_book/user/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your desired homepage URL
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your desired homepage URL
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
